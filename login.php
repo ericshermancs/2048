316075926fa8c8a20a9017d1cd01147c25173fdd
Eric Sherman <zensap92@gmail.com>
2020-04-19 16:40:53 -0400

    push login logout pages and die

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
    if(isset($_SESSION['username'])){
       header("location:index.php");
+      die();
    }
 
    include("scripts/config.php");
--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
 unset($_SESSION['username']);
 header("location:login.php");
+die();
 ?>
