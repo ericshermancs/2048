6ace66d53bfc0c49950774f43e662e27629a4298
Eric Sherman <zensap92@gmail.com>
2020-04-21 11:55:47 -0400

    migrate php to new page

---
+++ Login_v14/index.php
@@ -0,0 +1,169 @@
+<?php
+   session_start();
+   
+   if(isset($_SESSION['username'])){
+      header("location:/2048/index.php");
+      die();
+   }
+
+   include("/2048/scripts/config.php");
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = filter_var($_POST['username'], FILTER_SANITIZE_STRING);  
+      $mypassword = filter_var($_POST['password'], FILTER_SANITIZE_STRING);  
+      
+      //echo $myusername." ".$mypassword." ".$_POST['username']." ".$_POST['password']."<br>";
+
+      
+      if($_POST['login']==' Login '){
+         $sql = "SELECT * FROM users WHERE username = '".$myusername."' and password = '".$mypassword."'";
+      }
+      else if($_POST['login'] == ' Register '){
+         $sql = "SELECT * FROM users WHERE username = '".$myusername."'";
+      }
+      // echo $sql;
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+      if($count == 1){
+         if($_POST['login']==' Register '){
+            $error = "It appears that username is already taken";
+         }
+         else if($_POST['login']==' Login '){
+            $_SESSION['username'] = $myusername;
+            header("location: /2048/index.php");
+         }
+      }
+      else {
+         if($_POST['login']==' Login ')
+            $error = "Your Login Name or Password is invalid";
+         else if($_POST['login']==' Register '){
+            $sql = "INSERT INTO users(username,password) VALUES('".$myusername."','".$mypassword."');";
+            $result = mysqli_query($conn,$sql);
+            $_SESSION['username'] = $myusername;
+            header("location: /2048/index.php");
+         }
+      }
+   }
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>2x4=8</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+				<form class="login100-form validate-form flex-sb flex-w" action="/2048/login.php" method="POST">
+					<span class="login100-form-title p-b-32">
+						2x4=8
+					</span>
+
+					<span class="txt1 p-b-11">
+						Username
+					</span>
+					<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
+						<input class="input100" type="text" name="username" >
+						<span class="focus-input100"></span>
+					</div>
+					
+					<span class="txt1 p-b-11">
+						Password
+					</span>
+					<div class="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+						<span class="btn-show-pass">
+							<i class="fa fa-eye"></i>
+						</span>
+						<input class="input100" type="password" name="password" >
+						<span class="focus-input100"></span>
+					</div>
+					<!--
+					<div class="flex-sb-m w-full p-b-48">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>
+
+						<div>
+							<a href="#" class="txt3">
+								Forgot Password?
+							</a>
+						</div>
+					</div>
+					-->
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" name="login" value=" Login " style="margin:10px;">
+							Login
+						</button>
+						<button class="login100-form-btn" name="login" value=" Register " style="margin:10px;">
+							Register
+						</button>
+					</div>
+
+				</form>
+				<div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
